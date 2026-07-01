95028604e1c2f82e46a34864be94fef51334ce91
文 绍 郭 <790092318@qq.com>
2026-07-01 22:01:51 +0800

    🎉 重置：ECompany Asst v2.0 精洁推送

--- app/preload.js
+++
@@ -1,7 +0,0 @@
-// preload.js - Safely expose ipcRenderer to renderer
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  onSetLanguage: (callback) => ipcRenderer.on('set-language', (event, lang) => callback(lang)),
-  sendChangeLanguage: (lang) => ipcRenderer.send('change-language', lang)
-});
